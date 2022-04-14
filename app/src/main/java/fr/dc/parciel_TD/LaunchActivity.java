6b85f7047504b21f5144b4b16517a81559b7d6c2
Théau Dugueperoux <theaudugueperoux@gmail.com>
2022-04-14 10:34:45 +0200

    First page done

---
+++ app/src/main/java/fr/dc/parciel_TD/LaunchActivity.java
@@ -0,0 +1,8 @@
+package fr.dc.parciel_TD;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LaunchActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/fr/dc/parciel_TD/MainActivity.java
+++ app/src/main/java/fr/dc/parciel_TD/MainActivity.java
@@ -1,14 +1,47 @@
 package fr.dc.parciel_TD;
 
+import static java.lang.Integer.valueOf;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import fr.dc.parciel_TD.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding ui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
+
+        ui.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ui.nbDices.getText() != null | ui.nbFaces.getText() != null) {
+
+                    if (Integer.parseInt(ui.nbDices.getText().toString()) >= 1 & Integer.parseInt(ui.nbDices.getText().toString()) <= 6) {
+                        if (Integer.parseInt(ui.nbFaces.getText().toString()) >= 4 & Integer.parseInt(ui.nbFaces.getText().toString()) <= 12) {
+
+                        } else {
+                            ui.nbFaces.setText("");
+                            ui.nbFaces.setHint("Le nombre doit etre compris entre 4 et 12");
+                        }
+                    } else {
+                        ui.nbDices.setText("");
+                        ui.nbDices.setHint("Le nombre doit etre compris entre 1 et 6");
+                    }
+                }else{
+                    String toast = "Il faut des entres des valeurs dans toute les cases";
+                    Toast.makeText(MainActivity.this, toast, Toast.LENGTH_LONG).show();
+                    ui.nbFaces.setText("");
+                    ui.nbDices.setText("");
+                }
+            }
+        });
     }
 }
